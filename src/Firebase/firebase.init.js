9d68a839f9d00c504137e2e108402fb7176a5eb2
nahid-code1 <git config --global user.email nahidulislam.work@gmail.com>
2025-12-18 23:27:42 +0600

    added firebase config and environment variable

--- src/Firebase/firebase.init.js
+++ src/Firebase/firebase.init.js
@@ -1,7 +1,7 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
 // TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
+// https://firebase.google.com/docs/web/setup#available-librariees
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
